013fe698adfe62773915b0457e499161f1e1a5ec
tycheyoung <retbird13@gmail.com>
2023-06-08 05:14:08 +0000

    Specify numpy version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
             packages=find_packages(),
             include_package_data=True,
             install_requires=[
-                'numpy',
+                'numpy==1.12.1',
                 'astor',
                 'sympy==1.2',
                 'pyimage',
